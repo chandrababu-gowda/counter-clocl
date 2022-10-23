9fe094de72b2f01d9a9bf950b18598206a1b610d
Chandrababu Gowda <96734849+chandrababu-gowda@users.noreply.github.com>
2022-10-23 10:37:49 +0530

    Add the enterd time to the current time

--- index.js
+++ index.js
@@ -2,25 +2,36 @@
 // let minute = document.getElementById("minute");
 // let second = document.getElementById("second");
 
-// let date = new Date();
-// let time = date.getHours();
-// console.log(date);
-// console.log(time);
-
-
 let hour = document.getElementById('hour');
 let minute = document.getElementById('minute');
 let second = document.getElementById('second');
 let btn = document.getElementById("submit");
 
+
+
 btn.addEventListener('click', ()=>{
-    hourValue = hour.value;
-    minuteValue = minute.value;
-    secondValue = second.value;
-    console.log(hourValue);
-    console.log(minuteValue);
-    console.log(secondValue);
+    let date = new Date();
+    let currentHour = date.getHours();
+    let currentMinute = date.getMinutes();
+    let currentSecond = date.getSeconds();
+    hourValue = parseInt(hour.value);
+    minuteValue = parseInt(minute.value);
+    secondValue = parseInt(second.value);
+    let setHourValue = currentHour + hourValue ;
+    let setMinuteValue = currentMinute + minuteValue;
+    let setSecondValue = currentSecond + secondValue;
+
+    console.log(currentHour,currentMinute,currentSecond);
+    console.log(hourValue,minuteValue,secondValue);
+    console.log(setHourValue,setMinuteValue,setSecondValue);
+
+
+
 })
+
+
+
+
 
 
 
