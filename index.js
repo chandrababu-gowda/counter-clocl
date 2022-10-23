8073ea1c00937c9f2770a1229e3028c76604c9f7
Chandrababu Gowda <96734849+chandrababu-gowda@users.noreply.github.com>
2022-10-23 11:07:45 +0530

    Show alert when time is over

--- index.js
+++ index.js
@@ -1,13 +1,7 @@
-// let hour = document.getElementById("hour");
-// let minute = document.getElementById("minute");
-// let second = document.getElementById("second");
-
 let hour = document.getElementById('hour');
 let minute = document.getElementById('minute');
 let second = document.getElementById('second');
 let btn = document.getElementById("submit");
-
-
 
 btn.addEventListener('click', ()=>{
     let date = new Date();
@@ -21,11 +15,18 @@ btn.addEventListener('click', ()=>{
     let setMinuteValue = currentMinute + minuteValue;
     let setSecondValue = currentSecond + secondValue;
 
-    console.log(currentHour,currentMinute,currentSecond);
-    console.log(hourValue,minuteValue,secondValue);
-    console.log(setHourValue,setMinuteValue,setSecondValue);
+    for(i=0;i>=0;i++) {
+        let newDate = new Date();
+        let newCurrentHour = newDate.getHours();
+        let newCurrentMinute = newDate.getMinutes();
+        let newCurrentSecond = newDate.getSeconds();
+        let conditionCheck = newCurrentHour==setHourValue&&newCurrentMinute==setMinuteValue&&newCurrentSecond==setSecondValue
+        if(conditionCheck){
+            alert("Time's up!")
+            break;
+        }
 
-
+    }
 
 })
 
