cf74889e1c8ba9d800b41c3b93054adb94beebdd
Chandrababu Gowda <96734849+chandrababu-gowda@users.noreply.github.com>
2022-10-23 10:18:37 +0530

    Get the entered value

---
+++ index.js
@@ -0,0 +1,37 @@
+// let hour = document.getElementById("hour");
+// let minute = document.getElementById("minute");
+// let second = document.getElementById("second");
+
+// let date = new Date();
+// let time = date.getHours();
+// console.log(date);
+// console.log(time);
+
+
+let hour = document.getElementById('hour');
+let minute = document.getElementById('minute');
+let second = document.getElementById('second');
+let btn = document.getElementById("submit");
+
+btn.addEventListener('click', ()=>{
+    hourValue = hour.value;
+    minuteValue = minute.value;
+    secondValue = second.value;
+    console.log(hourValue);
+    console.log(minuteValue);
+    console.log(secondValue);
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
